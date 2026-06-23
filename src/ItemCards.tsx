5465efd8c5d714a96af6ac5f7b904bfd7acad6b1
armani-altaf <aaltaf@ybs.co.uk>
2026-06-23 14:40:40 +0100

    itemcard&db

---
+++ src/ItemCards.tsx
@@ -0,0 +1,16 @@
+
+
+
+function ItemCards() {
+
+
+
+    return (
+        <div className="item-card-container">
+            <img></img>
+            <p>Product Name</p>
+        </div>   
+    );
+}
+
+export default ItemCards;
