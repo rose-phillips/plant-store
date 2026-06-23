55b53f296a621b033ff34a80fbee2c835625fea3
armani-altaf <aaltaf@ybs.co.uk>
2026-06-23 16:10:46 +0100

    a million changes

--- src/components/AllProducts.tsx
+++ src/components/AllProducts.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
+import type { ProductInfoTypes } from '../types'
+import {ItemCards} from './ItemCards';
 
 export const AllProducts = () => {
+
+  const [productInfo, setProductInfo] = useState<ProductInfoTypes[]>([]);
+  console.log("productInfo")
+
+  //getdata
+  useEffect(() => {
+    fetch('http://localhost:3000/products')
+      .then((response) => response.json())
+      .then((data) => setProductInfo(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+
+
   return (
-    <div>AllProducts</div>
-  )
-}
+    <>
+      <div>AllProducts</div>
+      <div className='all-products-page'>
+      {productInfo.map((item) => (
+        <ItemCards {...item}/>
+      ))}
+      </div>
+      </>
+      )
+};
